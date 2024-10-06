ab8402fe7cc3868b4bfa8802c37b763ea71b4dc8
Enoch Kambale <campnoch@gmail.com>
2024-10-06 22:48:25 +0300

    first commit

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char message[BUFFER_SIZE] = {0};
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Creating socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    printf("Enter a message to send to the server: ");
+    fgets(message, BUFFER_SIZE, stdin);
+    message[strcspn(message, "\n")] = 0; // Remove newline character
+
+    send(sock, message, strlen(message), 0);
+    printf("Message sent\n");
+
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    printf("Server response: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
